a55c13c9b8b91714e1a0fd61c9ed721931473ffa
Anabell <gonzalez.anabell@gmail.com>
2016-10-20 16:57:48 -0400

    missing ID and Dept, all else is generating

--- courses/spiders/emerson.py
+++ courses/spiders/emerson.py
@@ -12,10 +12,17 @@ class EduSpider(Spider):
     start_urls = ['http://www.emerson.edu/academics/courses/descriptions']
 
     def parse(self, response):
-        lu = response.xpath('//ul[@id="courseList"]')
+        lu = response.xpath('//ul[@id="courseList"]/li')
+
+
 
         for li in lu:
             item = Course()
-            item['title'] = li.xpath('li/div[@id="BC110"]/div[1]/strong/text()').extract()[0]
+            item['site'] = 'www.emerson.edu'
+            item["institute"] = "Emerson College"
+            item['title'] = li.xpath('div/div[1]/strong/text()').extract()[0]
+            item['description'] = li.xpath('div/div/div[1]/text()').extract()[0]
+            item['credits'] = li.xpath('div/div[1]/em/text()').extract()[0]
+            item['id'] = li.xpath('li[@class="classHeader"]/text()').extract()[0]
             yield item
 
