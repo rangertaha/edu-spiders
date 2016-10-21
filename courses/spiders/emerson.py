bcce785864f474b853b06d0296bcec2b1ffb4c48
Anabell <gonzalez.anabell@gmail.com>
2016-10-21 16:59:48 -0400

    description generates but has <div> in text

--- courses/spiders/emerson.py
+++ courses/spiders/emerson.py
@@ -21,7 +21,10 @@ class EduSpider(Spider):
             item['site'] = 'www.emerson.edu'
             item["institute"] = "Emerson College"
             item['title'] = li.xpath('div/div[1]/strong/text()').extract()[0]
+            item['description'] = li.xpath('div/div[2]').extract()[0]
             item['credits'] = li.xpath('div/div[1]/em/text()').extract()[0]
             item['id'] = li.xpath('@data-id').extract()
             item['category'] = li.xpath('@data-deptcode').extract()
             yield item
+
+
