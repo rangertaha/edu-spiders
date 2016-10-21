7e5b97b4844ce469ff06cbd1ebeb37ce7ac8ecce
Anabell <gonzalez.anabell@gmail.com>
2016-10-21 15:56:40 -0400

    everything loading except description

--- courses/spiders/emerson.py
+++ courses/spiders/emerson.py
@@ -21,8 +21,7 @@ class EduSpider(Spider):
             item['site'] = 'www.emerson.edu'
             item["institute"] = "Emerson College"
             item['title'] = li.xpath('div/div[1]/strong/text()').extract()[0]
-            item['description'] = li.xpath('div/div/div[1]/text()').extract()[0]
             item['credits'] = li.xpath('div/div[1]/em/text()').extract()[0]
-            item['id'] = li.xpath('li[@class="classHeader"]/text()').extract()[0]
+            item['id'] = li.xpath('@data-id').extract()
+            item['category'] = li.xpath('@data-deptcode').extract()
             yield item
-
